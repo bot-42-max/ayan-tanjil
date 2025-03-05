5f5d70b7e1b47b2ee8ee0640df02477f919bb5dd
bot-42-max <ts9135246@gmail.com>
2025-03-05 21:32:32 +0600

    Update 00_prefix.js

--- scripts/cmds/00_prefix.js
+++ scripts/cmds/00_prefix.js
@@ -15,16 +15,16 @@ onChat: async function({ event, message, getLang }) {
 if (event.body && event.body.toLowerCase() === "prefix") {
 return message.reply({
 body: `
-‣ 𝐆𝐥𝐨𝐛𝐚𝐥 𝐩𝐫𝐞𝐟𝐢𝐱:. 
-‣𝐘𝐨𝐮𝐫 𝐠𝐫𝐨𝐮𝐩 𝐩𝐫𝐞𝐟𝐢𝐱:. 
+‣ 𝐆𝐥𝐨𝐛𝐚𝐥 𝐩𝐫𝐞𝐟𝐢𝐱: / 
+‣𝐘𝐨𝐮𝐫 𝐠𝐫𝐨𝐮𝐩 𝐩𝐫𝐞𝐟𝐢𝐱: /
 
-‣ 𝐀𝐝𝐦𝐢𝐧 
-‣ Ă̈Y̆̈Ă̈N̆̈ /Mahid
+‣ ᯽ 𝘼  𝐃  𝐌  𝐈  𝐍 ᯽
+‣    𝐓 𝐀 𝐍 𝐉 𝐈 𝐋
 
 ‣ 𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 ⓕ
-‣LaFie Ayan 
+‣ Ťåñjïł Hāšāń Æßp 
 \n\n\n  `,
-attachment: await global.utils.getStreamFromURL("https://i.imgur.com/yCkColX.jpeg")
+attachment: await global.utils.getStreamFromURL("https://i.imgur.com/WxtBNOq.jpeg")
 });
 }
 }
